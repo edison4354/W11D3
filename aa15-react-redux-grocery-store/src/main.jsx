8789e9278f9a54bb5232501e901451204cff442e
Edison <edison4354@gmail.com>
2024-05-15 11:59:13 -0700

    Phase 0 & 1 Setup

--- aa15-react-redux-grocery-store/src/main.jsx
+++ aa15-react-redux-grocery-store/src/main.jsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+import Data from './mockData/produce.json';
+
+const store = configureStore(Data);
+
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+    <Provider store={store}>
+        <App/>
+    </Provider>
   </React.StrictMode>
 );
 
